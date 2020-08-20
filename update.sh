a045b4194d7ea846d92bfdebb5940af8c0027ab1
kuraudo777 <ctuiwzfsewxbobcnhk@tsyefn.com>
2020-08-20 12:03:28 +0200

    Added newsboat config

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 cp i3config ~/.config/i3/config
 cp i3status.conf ~/.i3status.conf
-cp neoconfig ~/.config/neofetch/config
+#cp neoconfig ~/.config/neofetch/config
 cp Xdefaults ~/.Xdefaults
 cp mpd.conf ~/.mpd/mpd.conf
 #cp compton.conf ~/.compton/compton.conf
-cp userChrome.css ~/Desktop/chrome/
-
+#cp userChrome.css ~/Desktop/chrome/
+cp urls ~/.newsboat/urls
