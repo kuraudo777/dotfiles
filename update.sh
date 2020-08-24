69cc18b323b6e8dd13b8340d95a46fe2586b9312
kuraudo777 <kuraudo777@protonmail.com>
2020-08-24 20:54:24 +0200

    Added stuff

--- update.sh
+++ update.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 cp i3config ~/.config/i3/config
 cp i3status.conf ~/.i3status.conf
-#cp neoconfig ~/.config/neofetch/config
+cp neoconfig ~/.config/neofetch/config
 cp Xdefaults ~/.Xdefaults
+cp Xdefaults ~/.Xresources
 cp mpd.conf ~/.mpd/mpd.conf
 #cp compton.conf ~/.compton/compton.conf
 #cp userChrome.css ~/Desktop/chrome/
 cp urls ~/.newsboat/urls
 cp newsboatconf ~/.newsboat/config
 cp newsboatconf ~/.newsboat/config
+
+xrdb ~/.Xresources
